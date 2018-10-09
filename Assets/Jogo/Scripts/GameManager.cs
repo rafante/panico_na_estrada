340cd084c94b3d954ebe2f2d07ef980ccf091f8d
juscelino <juscelinotrevenzoli@gmail.com>
2018-10-09 13:28:24 -0300

    tela inicial

---
+++ Assets/Jogo/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+
+	public void Carregador(string name){
+		SceneManager.LoadScene (name);
+	}
+
+	
+}
